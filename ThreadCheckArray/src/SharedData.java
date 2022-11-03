06fd23128e0f38a60c5882feb042089cb7107abf
Rom Harel <Rom Harel@Roms-VivoBook>
2022-11-03 17:01:01 +0200

    Upload First Time

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,41 +1,77 @@
+import java.util.ArrayList;
+
+/**
+ * @author Rom Harel Noy Biton
+ * Class Shared Data  - Define the data that the threads share's
+ *
+ */
 public class SharedData 
 {
-	private int [] array;
+	private ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
 	
-	public SharedData(int[] array, int b) {
+	/**
+	 * @param array 
+	 * @param b
+	 * 
+	 * constructor 
+	 */
+	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
 
+	/**
+	 * @return
+	 * getter
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
-	public void setWinArray(boolean [] winArray) 
-	{
+	/**
+	 * @param winArray
+	 * getter
+	 */
+	public void setWinArray(boolean [] winArray) {
 		this.winArray = winArray;
 	}
 
-	public int[] getArray() 
+	/**
+	 * @return
+	 * getter for array
+	 */
+	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+	/**
+	 * @return
+	 * getter for b
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * @return]
+	 * getter for flag
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/**
+	 * @param flag
+	 * setter for flag
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -1,16 +1,35 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * @author Rom Harel
+ * 
+ * The program shows how, with the help of two threads, you can perform 2
+The tests on the 1-an organ (and only it) at the same time when:
+1. The first one who finds a solution reports to the second thread that it is a solution
+available.
+2. Each thread checks if the other thread found a solution at all
+Performing a recursive call.
+3. When a thread finds a solution, it reports through a complete array or
+Boolean chosen which members are in the solution.
+4. From the moment one of the threads finds a solution and starts writing it down
+The second thread will not write about the solution either.
+
+
+The solution relies on the fact that exactly two threads will be executed.
+ *
+ */
 public class TestThreadCheckArray {
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)) {
 			Thread thread1, thread2;
 			System.out.println("Enter array size");
 			int num  = input.nextInt();
-			int [] array = new int[num];
+			ArrayList<Integer> array = new ArrayList<>();
 			System.out.println("Enter numbers for array");
 			
 			for (int index = 0; index < num; index++) 
-				array[index] = input.nextInt();
+				array.add(index, input.nextInt());
 			
 			System.out.println("Enter number");
 			num = input.nextInt();
@@ -35,9 +54,9 @@ public class TestThreadCheckArray {
 				System.out.println("Sorry");
 				return;
 			}
-			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().length);
+			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().size());
 			System.out.print("I:    ");
-			for(int index = 0; index < sd.getArray().length ; index++)
+			for(int index = 0; index < sd.getArray().size() ; index++)
 				System.out.print(index + "    ");
 			System.out.println();
 			System.out.print("A:    ");
